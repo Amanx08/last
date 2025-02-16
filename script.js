0486abcdbe49db2a4a73617f2542cea61a34a884
Amanx08 <amanx03590@gmail.com>
2025-02-17 00:13:14 +0530

    add slider

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+// Slider 
+
+$(document).ready(function ($) {
+  $('.slider_container').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true,
+    responsive: [{
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+});
+
